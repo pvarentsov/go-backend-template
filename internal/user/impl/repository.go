b73e5b6d027b7cbb28cbcaf7e36378ca9c623f93
pvarentsov <varentsovpavel@gmail.com>
2022-05-07 17:45:07 +0300

    Code refactoring

--- internal/user/impl/repository.go
+++ internal/user/impl/repository.go
@@ -3,6 +3,9 @@ package impl
 import (
 	"context"
 
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgerrcode"
+
 	"go-backend-template/internal/base/errors"
 	"go-backend-template/internal/user"
 
@@ -138,4 +141,55 @@ func (r *userRepository) GetByEmail(ctx context.Context, email string) (user.Use
 	}
 
 	return model, nil
+}
+
+func parseAddUserError(user *user.UserModel, err error) error {
+	pgError, isPgError := err.(*pgconn.PgError)
+
+	if isPgError && pgError.Code == pgerrcode.UniqueViolation {
+		switch pgError.ConstraintName {
+		case "users_email_key":
+			return errors.Wrapf(errors.AlreadyExistsError, err, "user with email \"%s\" already exists", user.Email)
+		default:
+			return errors.Wrap(errors.DatabaseError, err, "add user failed")
+		}
+	}
+
+	return errors.Wrap(errors.DatabaseError, err, "add user failed")
+}
+
+func parseUpdateUserError(user *user.UserModel, err error) error {
+	pgError, isPgError := err.(*pgconn.PgError)
+
+	if isPgError && pgError.Code == pgerrcode.UniqueViolation {
+		return errors.Wrapf(errors.AlreadyExistsError, err, "user with email \"%s\" already exists", user.Email)
+	}
+
+	return errors.Wrap(errors.DatabaseError, err, "update user failed")
+}
+
+func parseGetUserByIdError(userId int64, err error) error {
+	pgError, isPgError := err.(*pgconn.PgError)
+
+	if isPgError && pgError.Code == pgerrcode.NoDataFound {
+		return errors.Wrapf(errors.NotFoundError, err, "user with id \"%d\" not found", userId)
+	}
+	if err.Error() == "no rows in result set" {
+		return errors.Wrapf(errors.NotFoundError, err, "user with id \"%d\" not found", userId)
+	}
+
+	return errors.Wrap(errors.DatabaseError, err, "get user by id failed")
+}
+
+func parseGetUserByEmailError(email string, err error) error {
+	pgError, isPgError := err.(*pgconn.PgError)
+
+	if isPgError && pgError.Code == pgerrcode.NoDataFound {
+		return errors.Wrapf(errors.NotFoundError, err, "user with email \"%s\" not found", email)
+	}
+	if err.Error() == "no rows in result set" {
+		return errors.Wrapf(errors.NotFoundError, err, "user with email \"%s\" not found", email)
+	}
+
+	return errors.Wrap(errors.DatabaseError, err, "get user by email failed")
 }
