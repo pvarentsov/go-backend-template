ec743745aac7c9c72d64fbc2188f6ddab8b0dada
pvarentsov <varentsovpavel@gmail.com>
2022-05-07 17:41:48 +0300

    Code refactoring

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -1,13 +1,20 @@
 //go:generate mockery --name AuthService --filename service.go --output ./mock --with-expecter
+//go:generate mockery --name Config --filename config.go --output ./mock --with-expecter
 
 package auth
 
 import (
 	"context"
+	"time"
 )
 
 type AuthService interface {
 	Login(ctx context.Context, dto LoginUserDto) (LoggedUserDto, error)
 	VerifyAccessToken(accessToken string) (int64, error)
 	ParseAccessToken(accessToken string) (int64, error)
+}
+
+type Config interface {
+	AccessTokenSecret() string
+	AccessTokenExpiresDate() time.Time
 }
